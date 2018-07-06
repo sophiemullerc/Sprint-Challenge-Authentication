9f7b1f7a542c38630963b8e16b7a8dd3b1a03ec6
sophiemullerc <32464200+sophiemullerc@users.noreply.github.com>
2018-07-06 15:02:27 -0500

    Complete Sprint Sophie Muller

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/auth-users', {
-  useMongoClient: true
-});
+mongoose.connect('mongodb://localhost/auth-users', {});
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
